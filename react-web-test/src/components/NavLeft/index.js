de1587566a4706816e3cccf4994877e88380a5e2
shiqiangdva <1114005726@qq.com>
2018-11-13 12:24:29 +0800

    左侧菜单组件开发
    对菜单组件内容进行 map循环递归的形式进行解析成官网效果, 同时编辑左侧菜单图标以及样式进行性定义

--- react-web-test/src/components/NavLeft/index.js
+++ react-web-test/src/components/NavLeft/index.js
@@ -1,10 +1,45 @@
 import React, {Component} from 'react';
+import { Menu, Icon } from 'antd';
+import MenuConfig from './../../config/menuConfig';
+import './index.less';
+const SubMenu = Menu.SubMenu;
 
 export default class NavLeft extends Component {
+
+    componentWillMount() {
+        const menuTreeNode = this.renderMenu(MenuConfig);
+
+        this.setState({
+            menuTreeNode
+        });
+    }
+
+    /**
+     * 菜单渲染
+     */
+    renderMenu = (data) => {
+        return data.map((item) => {
+            if (item.children) {
+                return (
+                    <SubMenu title={item.title} key={item.key}>
+                        { this.renderMenu(item.children) }
+                    </SubMenu>
+                )
+            }
+            return <Menu.Item key={item.key}>{item.title}</Menu.Item>
+        })
+    };
+
     render() {
         return(
             <div>
-                This is NavLeft
+                <div className="logo">
+                    <img src="/assets/logo-ant.svg" alt=""/>
+                    <h1>Imooc MS</h1>
+                </div>
+                <Menu theme="dark">
+                    {this.state.menuTreeNode}
+                </Menu>
             </div>
         )
     };
