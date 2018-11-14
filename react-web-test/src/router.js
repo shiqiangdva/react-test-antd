b7083805445b6143680e7a4355d333ef887cc1a9
shiqiangdva <1114005726@qq.com>
2018-11-14 20:39:12 +0800

    实现轮播图 效果

--- react-web-test/src/router.js
+++ react-web-test/src/router.js
@@ -11,6 +11,7 @@ import Notice from "./pages/ui/notice";
 import Message from "./pages/ui/message";
 import Tab from "./pages/ui/tabs";
 import Gallery from "./pages/ui/gallery";
+import Carousels from "./pages/ui/carousel";
 
 export default class IRouter extends Component {
 
@@ -30,6 +31,7 @@ export default class IRouter extends Component {
                                 <Route path="/admin/ui/messages" component={Message}></Route>
                                 <Route path="/admin/ui/tabs" component={Tab}></Route>
                                 <Route path="/admin/ui/gallery" component={Gallery}></Route>
+                                <Route path="/admin/ui/carousel" component={Carousels}></Route>
                                 <Route component={NoMatch}></Route>
                             </Switch>
                         </Admin>
