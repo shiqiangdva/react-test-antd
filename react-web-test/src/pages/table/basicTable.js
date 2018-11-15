5ea62024b9f953e4a9900bdb282f9e56295217d8
shiqiangdva <1114005726@qq.com>
2018-11-15 20:23:08 +0800

    基础表格的实现

--- react-web-test/src/pages/table/basicTable.js
+++ react-web-test/src/pages/table/basicTable.js
@@ -1,12 +1,91 @@
 import React, {Component} from 'react';
-import {} from 'antd';
+import {Card, Table} from 'antd';
 
 export default class BasicTable extends Component {
 
+    state = {};
+
+    componentDidMount() {
+        const dataSource = [
+            {
+                id: '0',
+                userName: 'Kevin0',
+                sex: '1',
+                state: '1',
+                interest: '1',
+                birthday: '1993-11-26',
+                address: '大连市沙河口区',
+                time: '09:00'
+            }, {
+                id: '1',
+                userName: 'Kevin1',
+                sex: '1',
+                state: '1',
+                interest: '1',
+                birthday: '1993-11-26',
+                address: '大连市沙河口区',
+                time: '09:00'
+            }, {
+                id: '2',
+                userName: 'Kevin2',
+                sex: '1',
+                state: '1',
+                interest: '1',
+                birthday: '1993-11-26',
+                address: '大连市沙河口区',
+                time: '09:00'
+            },
+        ];
+
+        this.setState({
+            dataSource
+        });
+
+    }
+
     render() {
+        // 表头
+        const columns = [
+            {
+                title: 'id',
+                dataIndex: 'id'
+            }, {
+                title: '用户名',
+                dataIndex: 'userName'
+            }, {
+                title: '性别',
+                dataIndex: 'sex'
+            }, {
+                title: '状态',
+                dataIndex: 'state'
+            }, {
+                title: '爱好',
+                dataIndex: 'interest'
+            }, {
+                title: '生日',
+                dataIndex: 'birthday'
+            }, {
+                title: '地址',
+                dataIndex: 'address'
+            }, {
+                title: '早起时间',
+                dataIndex: 'time'
+            }
+
+        ];
+
         return(
             <div>
-
+                <Card>
+                    <Table
+                        // 带边框
+                        bordered
+                        columns={columns}
+                        dataSource={this.state.dataSource}
+                        // 不要分页
+                        pagination={false}
+                    />
+                </Card>
             </div>
         )
     }
--- react-web-test/src/router.js
+++ react-web-test/src/router.js
@@ -14,6 +14,7 @@ import Gallery from "./pages/ui/gallery";
 import Carousels from "./pages/ui/carousel";
 import FormLogin from "./pages/form/login";
 import FormRegister from "./pages/form/register";
+import BasicTable from "./pages/table/basicTable";
 
 export default class IRouter extends Component {
 
@@ -37,6 +38,7 @@ export default class IRouter extends Component {
 
                                 <Route path="/admin/form/login" component={FormLogin}></Route>
                                 <Route path="/admin/form/reg" component={FormRegister}></Route>
+                                <Route path="/admin/table/basic" component={BasicTable}></Route>
 
                                 <Route component={NoMatch}></Route>
                             </Switch>
